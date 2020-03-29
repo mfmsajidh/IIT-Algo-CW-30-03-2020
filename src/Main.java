f721887301a2ce3e029292b53dbd29c3ab1403d2
Sajidh Farook <3187628-mfmsajidh@users.noreply.gitlab.com>
2020-03-30 03:38:33 +0530

    Added sample files

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    // Driver program to test above functions
+    public static void main(String[] args) throws java.lang.Exception {
+        // Let us create a graph shown in the above example
+        int graph[][] = new int[][]{{0, 16, 13, 0, 0, 0},
+                {0, 0, 10, 12, 0, 0},
+                {0, 4, 0, 0, 14, 0},
+                {0, 0, 9, 0, 0, 20},
+                {0, 0, 0, 7, 0, 4},
+                {0, 0, 0, 0, 0, 0}
+        };
+        MaxFlow m = new MaxFlow();
+
+        System.out.println("The maximum possible flow is " +
+                m.fordFulkerson(graph, 0, 5));
+
     }
 }
